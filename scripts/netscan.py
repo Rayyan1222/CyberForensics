52e7467e2236aebe3156ee4fa05b331f22969e09
Rayyan1222 <156992065+Rayyan1222@users.noreply.github.com>
2025-10-19 17:58:14 -0500

    Add netscan data processing script
    This script processes a netscan output file, cleans the data, and saves it as a CSV.

---
+++ scripts/netscan.py
@@ -0,0 +1,32 @@
+import pandas as pd
+#List to store row, header data
+rows = []
+header = []
+#Flag to indicate header was found
+found = False
+#Open they netscan output file
+with open("netscan.txt", "r") as f:
+    for line in f:
+        line = line.strip()
+        #Skip empty lines or line that start with volatility
+        if line == "" or line.startswith("Volatility"):
+            continue
+        #Split line into seperate values
+        parts = line.split()
+        #Detect header row, iff oofset store it once.
+        if parts[0] == "Offset" and not found:
+            header = parts
+            found = True
+            continue
+        #Process data rows only after header is found
+        if found:
+            #Make sure header is more than parts
+            while len(parts) < len(header):
+                parts.append("")
+            #Only take up to the number of header columns
+            rows.append(parts[:len(header)])
+#Create Dataframe with rows and columns
+df = pd.DataFrame(rows, columns=header)
+df.to_csv("netscan_cleaned.csv", index=False)
+print("Done")
+

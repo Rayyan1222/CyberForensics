29debbbb9b659f16e09af9e4c67fe948b83eea7a
Rayyan1222 <156992065+Rayyan1222@users.noreply.github.com>
2025-10-19 17:47:21 -0500

    Create psscan.py

---
+++ scripts/psscan.py
@@ -0,0 +1,22 @@
+import pandas as pd
+
+#List each row of process data and column headers
+rows = []
+header = []
+#Open and read psscan output file
+with open("psscan.txt", "r") as f:
+    for line in f:
+        line = line.strip()
+        #Skip empty lines or name start with Volatility
+        if line == "" or line.startswith("Volatility"):
+            continue
+        parts = line.split()
+        #Detect header row is 'PID' and store first 5 columns as headers
+        if parts[0] == "PID":
+            header = parts[:5]    
+            continue
+        rows.append(parts[:5])  
+#Create a DataFrame using rows with column names.
+df = pd.DataFrame(rows, columns=header)
+df.to_csv("psscan_cleaned.csv", index=False)
+print("Done")
